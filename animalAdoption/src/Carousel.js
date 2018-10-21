93612c3c0c4ce8487c0734684d31fcf57adb6503
Mark Suzuki <markdsuzuki@gmail.com>
2018-10-21 09:09:14 -0700

    styled components

--- animalAdoption/src/Carousel.js
+++ animalAdoption/src/Carousel.js
@@ -1,25 +1,23 @@
-import React , { Component } from 'react'
+import React from "react";
 
-class Carousel extends Component {
+class Carousel extends React.Component {
   state = {
     photos: [],
     active: 0
-  }
-
+  };
   static getDerivedStateFromProps({ media }) {
-    let photos
+    let photos = [];
     if (media && media.photos && media.photos.photo) {
       photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
     }
+
     return { photos };
   }
-
   handleIndexClick = event => {
     this.setState({
       active: +event.target.dataset.index
     });
-  }
-
+  };
   render() {
     const { photos, active } = this.state;
 
@@ -28,25 +26,25 @@ class Carousel extends Component {
       hero = photos[active].value;
     }
 
-    return(
+    return (
       <div className="carousel">
-      <img src={hero} alt="animal" />
-      <div className="carousel-smaller">
-        {photos.map((photo, index) => (
-          /* eslint-disable-next-line */
-          <img
-            onClick={this.handleIndexClick}
-            data-index={index}
-            key={photo.value}
-            src={photo.value}
-            className={index === active ? "active" : ""}
-            alt="animal thumbnail"
-          />
-        ))}
+        <img src={hero} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo.value}
+              src={photo.value}
+              className={index === active ? "active" : ""}
+              alt="animal thumnbail"
+            />
+          ))}
+        </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
-export default Carousel
+export default Carousel;
--- animalAdoption/src/Details.js
+++ animalAdoption/src/Details.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import pf from 'petfinder-client';
+import React from "react";
+import pf from "petfinder-client";
 import { navigate } from "@reach/router";
-import Carousel from './Carousel'
+import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
-})
+});
 
-class Details extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true
-    }
-  }
-
+class Details extends React.Component {
+  state = { loading: true, showModal: false };
   componentDidMount() {
     petfinder.pet
       .get({
@@ -23,20 +18,20 @@ class Details extends Component {
         id: this.props.id
       })
       .then(data => {
-        const pet = data.petfinder.pet;
         let breed;
-        if (Array.isArray(pet.breeds.breed)) {
-          breed = pet.breeds.breed.join(", ");
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = data.petfinder.pet.breeds.breed.join(", ");
         } else {
-          breed = pet.breeds.breed;
+          breed = data.petfinder.pet.breeds.breed;
         }
-
         this.setState({
-          name: pet.name,
-          animal: pet.animal,
-          location: `${pet.contact.city}, ${pet.contact.state}`,
-          description: pet.description,
-          media: pet.media,
+          name: data.petfinder.pet.name,
+          animal: data.petfinder.pet.animal,
+          location: `${data.petfinder.pet.contact.city}, ${
+            data.petfinder.pet.contact.state
+          }`,
+          description: data.petfinder.pet.description,
+          media: data.petfinder.pet.media,
           breed,
           loading: false
         });
@@ -45,27 +40,43 @@ class Details extends Component {
         navigate("/");
       });
   }
-
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
   render() {
     if (this.state.loading) {
-      return <h1>loading …</h1>;
+      return <h1>loading … </h1>;
     }
 
-    const { name, animal, breed, location, description, media } = this.state;
+    const {
+      media,
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
-      <Carousel media={media}/>
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
-          <h2>
-            {animal} – {breed} – {location}
-          </h2>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Details
+export default Details;
